39aa22b178f277040d4206de3d4f373331c364fa
wararaki <ky7.ott.w@gmail.com>
2022-07-24 23:20:34 +0900

    update

--- py_sample/fastapi_di_model/src/api/main.py
+++ py_sample/fastapi_di_model/src/api/main.py
@@ -11,11 +11,14 @@ from .schema import Result
 
 app = FastAPI()
 config = Config.load(Path("api/config.yml"))
-
+get_model = partial(Classifier.get_model, config)
 
 @app.get("/{value}", response_model=Result)
-def predict(value: int, model: Optional[Classifier] = Depends(partial(Classifier.get_model, config))):
+def predict(value: int, model: Optional[Classifier] = Depends(get_model)):
     if model is None:
         return Result(value=-1)
+    print(value)
     result = model.predict(value)
+    print(model._threshold)
+    print(app.dependency_overrides.keys())
     return Result(value=result)
---
+++ py_sample/fastapi_di_model/src/tests/test_main.py
@@ -0,0 +1,27 @@
+from functools import partial
+
+import pytest
+from fastapi.testclient import TestClient
+
+from api.config import Config
+from api.main import app, get_model
+from api.model import Classifier
+
+@pytest.fixture
+def client() -> TestClient:
+    api = TestClient(app)
+    app.dependency_overrides[get_model] = partial(Classifier.get_model, Config(threshold=1))
+    yield api
+    app.dependency_overrides = {}
+
+
+def test_predict_one(client: TestClient):
+    response = client.get("/1")
+    assert response.status_code == 200
+    assert response.json() == {"value": 1}
+
+
+def test_predict_zero(client: TestClient):
+    response = client.get("/0")
+    assert response.status_code == 200
+    assert response.json() == {"value": 0}
