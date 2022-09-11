9181b07f83be108dc9a7dad0fa2d6a9842c3d736
wararaki <ky7.ott.w@gmail.com>
2022-09-11 23:49:52 +0900

    update

--- ml_sample/hnsw/utils.py
+++ ml_sample/hnsw/utils.py
@@ -9,3 +9,14 @@ def show(node: Node):
 
 def euclidean(x: np.ndarray, y: np.ndarray) -> float:
     return np.linalg.norm(x - y)
+
+
+def random_level(ml: float) -> int:
+    uniform = np.random.uniform()
+    return int(np.floor(-np.log(uniform)*(1/np.log(ml))))
+
+
+if __name__ == "__main__":
+    for i in range(2, 6):
+        lv = random_level(i)
+        print(lv)
