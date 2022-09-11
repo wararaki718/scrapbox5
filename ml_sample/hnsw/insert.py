306f40bd0d2f2f133cd48a856655e9e21f8083b3
wararaki <ky7.ott.w@gmail.com>
2022-09-12 00:19:23 +0900

    update

--- ml_sample/hnsw/insert.py
+++ ml_sample/hnsw/insert.py
@@ -8,6 +8,7 @@ from node import Node, NodeDistCloser
 from search_layer import search_layer
 from select_neighbors import select_neighbors_simple
 from treeset import TreeSet
+from utils import random_level
 
 
 # ref: https://github.com/rust-cv/hnsw/blob/master/src/hnsw/hnsw_const.rs#L144
@@ -23,3 +24,12 @@ def insert(layers: List[Layer], q: np.ndarray, m: int, m_max: int, ef: int, ml: 
     # implement empty case
     # 1. first insert
     # 2. create new layer
+    l = random_level(ml)
+    if l >= len(layers):
+        cap = ef
+    else:
+        cap = 1
+    
+    
+    
+    
