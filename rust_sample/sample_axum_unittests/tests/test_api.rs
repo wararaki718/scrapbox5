1bbbe427fcede90945e45393112dca4fce735446
wararaki <ky7.ott.w@gmail.com>
2022-10-03 19:34:27 +0900

    fix

--- rust_sample/sample_axum_unittests/tests/test_api.rs
+++ rust_sample/sample_axum_unittests/tests/test_api.rs
@@ -1,32 +1,8 @@
-use axum::{
-    body::{Body, BoxBody},
-    http::{Request, Response, StatusCode, Method}
-};
-use tower::{Service, ServiceExt};
-use std::convert::Infallible;
+use axum::http::{StatusCode, Method};
 
+mod utils;
+use utils::call_service;
 use sample_axum_unittests::app::get_app;
-
-
-async fn call_service<S>(
-    svc: &mut S,
-    method: Method,
-    uri: &str,
-) -> (StatusCode, String)
-where
-    S: Service<Request<Body>, Response = Response<BoxBody>, Error = Infallible>
-{
-    let req = Request::builder().method(method).uri(uri).body(Body::empty()).unwrap();
-    let res = svc.ready().await.unwrap().call(req).await.unwrap();
-
-    let status = res.status();
-
-    let body = res.into_body();
-    let body = hyper::body::to_bytes(body).await.unwrap();
-    let body = String::from_utf8(body.to_vec()).unwrap();
-
-    (status, body)
-}
 
 
 #[tokio::test]
