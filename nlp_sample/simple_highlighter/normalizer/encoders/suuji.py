00853e21d728dee16024327076eb93ecb024ae86
wararaki <ky7.ott.w@gmail.com>
2022-11-09 19:44:54 +0900

    update

--- nlp_sample/simple_highlighter/normalizer/encoders/suuji.py
+++ nlp_sample/simple_highlighter/normalizer/encoders/suuji.py
@@ -8,6 +8,6 @@ class SuujiEncoder:
         n = len(tokens)
         for i in range(n-1, -1, -1):
             if str(tokens[i]) == "X":
-                tokens[i].fixed_char = "1"
-                tokens.insert(i+1, Character(base_index=tokens[i].base_index, fixed_char="0"))
+                tokens[i].fixed_char = "10"
+
         return tokens
--- nlp_sample/simple_highlighter/searcher.py
+++ nlp_sample/simple_highlighter/searcher.py
@@ -11,15 +11,15 @@ class PositionSearcher:
         """
         positions = []
         base = "".join(map(str, base_tokens))
-        target = "".join(map(str, target_tokens))
 
-        #print(base)
-        #print(target)
-        for i in range(len(target)-len(base)+1):
-            if target[i:i+len(base)] == base:
+        for i in range(len(target_tokens)-len(base_tokens)+1):
+            target = "".join(map(str, target_tokens[i:i+len(base_tokens)-1]))
+            #print(base, target[:len(base)], target[:len(base)]==base, len(target) - len(base))
+            if target[:len(base)] == base:
+                offset = len(target) - len(base)
                 position = Position(
                     start = target_tokens[i].base_index,
-                    end = target_tokens[i+len(base)].base_index
+                    end = target_tokens[i+len(base_tokens)-1-offset].base_index
                 )
                 positions.append(position)
 
