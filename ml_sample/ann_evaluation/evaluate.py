9b0728d6a2e3937700068a55fe1fb1856f4ffb3b
Kyohei Watarai <>
2022-11-17 10:44:08 +0900

    update

--- ml_sample/ann_evaluation/evaluate.py
+++ ml_sample/ann_evaluation/evaluate.py
@@ -1,12 +1,46 @@
 import numpy as np
 
 
-def accuracy_at_n(y_trues: np.ndarray, y_preds: np.ndarray, at_n: int=10) -> float:
-    if y_trues.shape != y_preds.shape:
-        raise TypeError("shape error")
-    
+def accuracy_at_k(y_trues: np.ndarray, y_preds: np.ndarray, k: int=10) -> float:
     total = 0
     for y_true, y_pred in zip(y_trues, y_preds):
-        total += len(set(y_true[:at_n])&set(y_pred[:at_n]))
+        total += len(np.intersect1d(y_true[:k], y_pred[:k]))
     
-    return total / (y_trues.shape[0]*min(at_n, y_trues.shape[1]))
+    return total / (y_trues.shape[0]*min(k, y_trues.shape[1]))
+
+
+def precision_at_k(y_trues: np.ndarray, y_preds: np.ndarray, k: int=10) -> float:
+    total = 0.0
+    for y_true, y_pred in zip(y_trues, y_preds):
+        total += len(np.intersect1d(y_true[:k], y_pred[:k])) / len(y_true[:k])
+    return total / len(y_trues)
+
+
+def recall_at_k(y_trues: np.ndarray, y_preds: np.ndarray, k: int=10) -> float:
+    total = 0.0
+    for y_true, y_pred in zip(y_trues, y_preds):
+        total += len(np.intersect1d(y_true[:k], y_pred[:k])) / len(y_true)
+    return total / len(y_trues)
+
+
+def f_measure_at_k(y_trues: np.ndarray, y_preds: np.ndarray, k: int=10) -> float:
+    recall = recall_at_k(y_trues, y_preds, k)
+    precision = precision_at_k(y_trues, y_preds, k)
+    return (2.0 * recall * precision) / (recall + precision)
+
+
+def mrr_at_k(y_trues: np.ndarray, y_preds: np.ndarray, k: int=10) -> float:
+    total = 0.0
+    for y_true, y_pred in zip(y_trues, y_preds):
+        for i, y in enumerate(y_pred[:k], start=1):
+            if y in y_true:
+                total += (1.0 / i)
+                break
+    return total / len(y_trues)
+
+
+def map_at_k(y_trues: np.ndarray, y_preds: np.ndarray, k: int=10) -> float:
+    total = 0.0
+    for i in range(1, k+1):
+        total += precision_at_k(y_trues, y_preds, i)
+    return total / len(y_trues)
--- ml_sample/ann_evaluation/main.py
+++ ml_sample/ann_evaluation/main.py
@@ -1,9 +1,26 @@
 import gc
 
+import numpy as np
+
 from ann import Flat, HNSW
 from config import ANNConfig
-from evaluate import accuracy_at_n
+from evaluate import accuracy_at_k, precision_at_k, recall_at_k, f_measure_at_k, mrr_at_k, map_at_k
 from utils import get_data, get_label
+
+
+def show(y_trues: np.ndarray, y_preds: np.ndarray, k: int=10):
+    #accuracy = accuracy_at_k(y_trues, y_preds, k=k)
+    precision = precision_at_k(y_trues, y_preds, k=k)
+    recall = recall_at_k(y_trues, y_preds, k=k)
+    f_measure = f_measure_at_k(y_trues, y_preds, k=k)
+    mrr = mrr_at_k(y_trues, y_preds, k=k)
+    map_ = map_at_k(y_trues, y_preds, k=k)
+    #print(f"accuracy@{k}  : {accuracy}")
+    print(f"precision@{k} : {precision}")
+    print(f"recall@{k}    : {recall}")
+    print(f"f_measure@{k} : {f_measure}")
+    print(f"mrr@{k}       : {mrr}")
+    print(f"map@{k}       : {map_}")
 
 
 def main():
@@ -17,16 +34,16 @@ def main():
     X_test = get_data(n_test, config.n_dimensions)
     
     l2 = Flat(config.n_dimensions, config.metric_type)
-    y_test = get_label(X_train, X_test, l2, top_n=1)
+    y_test = get_label(X_train, X_test, l2, top_n=10)
     del l2
     gc.collect()
 
     hnsw = HNSW(**config.to_dict())
     hnsw.add(X_train)
-    _, y = hnsw.search(X_test, k=1)
+    _, y = hnsw.search(X_test, k=5)
 
-    accuracy = accuracy_at_n(y_test, y, at_n=1)
-    print(f"hnsw accuracy: {accuracy}")
+    print("[hnsw]")
+    show(y_test, y, k=5)
 
     print("DONE")
 
