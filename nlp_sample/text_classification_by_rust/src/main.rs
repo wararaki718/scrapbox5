65fac446de43964e3915454f4f5428ae39a64276
wararaki <ky7.ott.w@gmail.com>
2022-11-30 17:24:15 +0900

    update

--- nlp_sample/text_classification_by_rust/src/main.rs
+++ nlp_sample/text_classification_by_rust/src/main.rs
@@ -14,7 +14,8 @@ fn main() {
     let text_analyzer = TextAnalyzer::new();
     let article_loader = ArticleLoader::new();
 
-    let articles = article_loader.load("it-life-hack");
+    let label = "it-life-hack";
+    let articles = article_loader.load(label);
     let mut texts = Vec::new();
     for article in &articles {
         let text = text_analyzer.analyze(&article.content);
@@ -28,7 +29,9 @@ fn main() {
     //println!("{:?}", vectorizer.vocabulary());
     
     let vecs = vectorizer.transform(&x);
+    let labels = vec![label; vecs.rows()];
     println!("{:?}", vecs.shape());
+    println!("{}", labels.len());
 
     println!("DONE");
 }
