93c66babf254f8f71387e6d3e5880d3e1101dee0
Kyohei Watarai <>
2022-12-01 10:47:46 +0900

    update

--- py_sample/poet_with_pybind11/build.py
+++ py_sample/poet_with_pybind11/build.py
@@ -1,21 +1,19 @@
-from pybind11 import get_include
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
+__version__ = "0.0.1"
 
 def build(setup_kwargs: dict):
     ext_modules = [
         Pybind11Extension(
-            "pybind11_extension",
+            "hello",
             ["cpp/hello.cpp"],
-            include_dirs=[
-                get_include()
-            ],
+            define_macros = [('VERSION_INFO', __version__)],
             languate="c++"
         )
     ]
 
     setup_kwargs.update({
         "ext_modules": ext_modules,
-        "cmd_class": {"build_ext": build_ext},
+        "cmdclass": {"build_ext": build_ext},
         "zip_safe": False
     })
