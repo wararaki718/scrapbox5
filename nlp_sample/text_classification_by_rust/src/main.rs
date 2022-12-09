1bee5358737e2340747e839efadc63172e7a0e91
wararaki <ky7.ott.w@gmail.com>
2022-12-09 20:58:17 +0900

    update

--- nlp_sample/text_classification_by_rust/src/main.rs
+++ nlp_sample/text_classification_by_rust/src/main.rs
@@ -32,31 +32,35 @@ fn main() {
     let article_loader = ArticleLoader::new();
 
     let (it_texts, it_labels) = load("it-life-hack", &text_analyzer, &article_loader);
-    let (sports_texts, sports_labels) = load("sports_watch", &text_analyzer, &article_loader);
+    let (sports_texts, sports_labels) = load("sports-watch", &text_analyzer, &article_loader);
     let texts = [it_texts, sports_texts].concat();
     let labels = [it_labels, sports_labels].concat();
-    let (text_train, text_test, label_train, label_test) = train_test_split(texts, labels, Some(0.7 as f32), Some(true));
+    println!("data loaded");
 
+    let (text_train, text_test, label_train, label_test) = train_test_split(texts, labels, Some(0.7 as f32), Some(true));
     let x_train = Array::from_vec(text_train);
     let x_test = Array::from_vec(text_test);
     let y_train = Array::from_vec(label_train);
     let y_test = Array::from_vec(label_test);
-
     //println!("{:?}", x);
     println!("{}, {:?}", x_train.ndim(), x_train.dim());
     println!("{:?}", y_train.shape());
     
     let vectorizer = TfIdfVectorizer::default().fit(&x_train).unwrap();
     //println!("{:?}", vectorizer.vocabulary());
+    println!("vectorized");
+
     let train_features = vectorizer.transform(&x_train);
-    let test_features = vectorizer.transform(&x_test);
-
     let train = Dataset::new(train_features.to_dense(), y_train);
-    let test = Dataset::new(test_features.to_dense(), y_test);
+    let model = LogisticRegression::default().max_iterations(50).fit(&train).unwrap();
+    println!("trained");
 
-    let model = LogisticRegression::default().max_iterations(150).fit(&train).unwrap();
+    let test_features = vectorizer.transform(&x_test);
+    let test = Dataset::new(test_features.to_dense(), y_test);
     let preds = model.predict(&test);
+    //let preds = model.predict(&train);
     let cm = preds.confusion_matrix(&test).unwrap();
+    //let cm = preds.confusion_matrix(&train).unwrap();
 
     println!("{:?}", cm);
     println!("accuracy: {}, MCC: {}", cm.accuracy(), cm.mcc());
