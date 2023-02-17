27de99ca5c378da18139a9fb353488a9e4c2f7c6
wararaki <ky7.ott.w@gmail.com>
2023-02-17 23:07:08 +0900

    update

---
+++ rust_sample/webapp_tutorial/README.md
@@ -0,0 +1,15 @@
+# webapp
+
+## build
+
+```shell
+cargo build
+```
+
+## run
+
+```shell
+cargo run
+```
+
+open http://127.0.0.1:3000 on your browser.
---
+++ rust_sample/webapp_tutorial/src/main.rs
@@ -0,0 +1,23 @@
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "webapp=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
