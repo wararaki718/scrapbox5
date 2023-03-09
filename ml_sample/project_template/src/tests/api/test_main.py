1fd90d1e9b4aa17b1ab05e1317cd867cab9671d3
Kyohei Watarai <>
2023-03-09 10:58:06 +0900

    update

--- ml_sample/project_template/src/tests/api/test_main.py
+++ ml_sample/project_template/src/tests/api/test_main.py
@@ -1,25 +1,25 @@
-# from typing import Generator
-# from unittest.mock import patch, MagicMock
+from typing import Generator
+from unittest.mock import patch, MagicMock
 
-# import numpy as np
-# import pytest
-# from fastapi.testclient import TestClient
-
-# from project_template.api.main import app
+import numpy as np
+import pytest
+from fastapi.testclient import TestClient
 
 
-# @pytest.fixture
-# def client() -> Generator[TestClient, None, None]:
-#     model = MagicMock()
-#     model.predict = MagicMock(return_value=np.array([[0]]))
-#     vectorizer = MagicMock()
-#     vectorizer.transform = MagicMock(return_value=np.array([0]))
-#     with patch("project_template.api.estimator.estimator.joblib.load", return_value=model):
-#         with patch("project_template.api.vectorizer.components.embarked.joblib.load", return_value=vectorizer):
-#             yield TestClient(app)
+@pytest.fixture
+def client() -> Generator[TestClient, None, None]:
+    model = MagicMock()
+    model.predict = MagicMock(return_value=np.array([[0]]))
+    vectorizer = MagicMock()
+    vectorizer.transform = MagicMock(return_value=np.array([0]))
+    with patch("project_template.api.estimator.estimator.joblib.load", return_value=model):
+        with patch("project_template.api.vectorizer.components.embarked.joblib.load", return_value=vectorizer):
+            ## TODO refactor
+            from project_template.api.main import app
+            yield TestClient(app)
 
 
-# def test_ping(client: TestClient) -> None:
-#     response = client.get("/ping")
-#     assert response.status_code == 200
-#     assert response.json() == "pong"
+def test_ping(client: TestClient) -> None:
+    response = client.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == "pong"
---
+++ ml_sample/project_template/src/tests/api/test_predictor.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from typing import Generator
+from unittest.mock import patch, MagicMock
+
+import numpy as np
+import pytest
+
+from project_template.api.predictor import SurvivePredictor
+
+
+@pytest.fixture
+def predictor() -> Generator[SurvivePredictor, None, None]:
+    config = MagicMock()
+    config.vectorizer_config = MagicMock()
+    config.vectorizer_config.vectorizer_path = Path("dummy")
+    config.estimator_config = MagicMock()
+    config.estimator_config.model_path = Path("dummy")
+
+    embarked_vectorizer = MagicMock()
+    embarked_vectorizer.transform = MagicMock(return_value=np.array([[1]]))
+
+    model = MagicMock()
+    model.predict = MagicMock(return_value=np.array([1]))
+
+    with patch("project_template.api.vectorizer.components.embarked.joblib.load", return_value=embarked_vectorizer):
+        with patch("project_template.api.estimator.estimator.joblib.load", return_value=model):
+            yield SurvivePredictor(config)
+
+@pytest.fixture
+def passenger() -> MagicMock:
+    passenger = MagicMock()
+    passenger.Pclass = 1
+    passenger.Age = 11
+    passenger.Embarked = "C"
+    return passenger
+
+
+# def test_predict(predictor: SurvivePredictor, passenger: MagicMock):
+#     result = predictor.predict(passenger)
+#     assert result == 1
