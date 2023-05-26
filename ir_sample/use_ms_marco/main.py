a80880c48b071e95f0f06880b4fbc1b45187c476
wararaki <ky7.ott.w@gmail.com>
2023-05-26 17:43:21 +0900

    update

--- ir_sample/use_ms_marco/main.py
+++ ir_sample/use_ms_marco/main.py
@@ -1,9 +1,12 @@
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
 
 
 def main():
-    data = load_dataset("ms_marco", "v2.1")
+    # data = load_dataset("ms_marco", "v2.1")
+    data: DatasetDict = load_dataset("ms_marco", "v1.1")
     print(type(data))
+    print(data.shape)
+    print(data["test"])
     print("DONE")
 
 
