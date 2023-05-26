1410e4dc513656c815dcaba58d7c3261ec7212f8
wararaki <ky7.ott.w@gmail.com>
2023-05-26 17:54:07 +0900

    update

--- ir_sample/use_ms_marco/main.py
+++ ir_sample/use_ms_marco/main.py
@@ -1,12 +1,20 @@
-from datasets import load_dataset, DatasetDict
+from datasets import load_dataset
+from datasets.arrow_dataset import Dataset
 
 
 def main():
     # data = load_dataset("ms_marco", "v2.1")
-    data: DatasetDict = load_dataset("ms_marco", "v1.1")
+    data: Dataset = load_dataset("ms_marco", "v1.1")
     print(type(data))
     print(data.shape)
     print(data["test"])
+    print(type(data["test"]))
+    test: Dataset = data["test"]
+    for column in test.features:
+        print(f"[{column}]:")
+        print(test[column][0])
+        print()
+        
     print("DONE")
 
 
