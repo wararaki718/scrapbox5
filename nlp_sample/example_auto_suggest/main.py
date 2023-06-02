fd798ac4503c09bc5f45894eeb8d81d2692717bd
wararaki <ky7.ott.w@gmail.com>
2023-06-02 20:38:47 +0900

    update

--- nlp_sample/example_auto_suggest/main.py
+++ nlp_sample/example_auto_suggest/main.py
@@ -1,4 +1,43 @@
+from suggester import BinarySearchSuggester, BruteForceSuggester, EdgeNGramSuggester, NGramSuggester, TrieTreeSuggester
+
+
 def main():
+    suggestions = [
+        "banana",
+        "baloon",
+        "baby",
+        "bad",
+        "bag",
+        "byte",
+        "bat",
+        "but",
+        "bug",
+        "ban",
+        "bound",
+        "bowl",
+        "boss",
+        "bottom",
+        "boom",
+        "both"
+    ]
+
+    suggesters = {
+        "binary_search": BinarySearchSuggester(),
+        "brute_force": BruteForceSuggester(),
+        "edge_ngram": EdgeNGramSuggester(),
+        "ngram": NGramSuggester(),
+        "trie_tree": TrieTreeSuggester()
+    }
+    
+    for name, suggester in suggesters.items():
+        print(f"[{name} suggester]")
+        suggester.index(suggestions)
+        for prefix in ["bo", "ba"]:
+            print(f"prefix: {prefix}")
+            for result in suggester.search(prefix):
+                print(f" - {result}")
+            print()
+
     print("DONE")
 
 
