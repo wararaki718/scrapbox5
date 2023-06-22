4a2acf0ae331f8c016a07cf22e2c64ccbd0b412b
wararaki <ky7.ott.w@gmail.com>
2023-06-22 23:03:24 +0900

    update

---
+++ ml_sample/siamese_network/README.md
@@ -0,0 +1,13 @@
+# Siamese Network
+
+## setup
+
+```shell
+pip install torch
+```
+
+## run
+
+```shell
+python main.py
+```
---
+++ ml_sample/siamese_network/main.py
@@ -0,0 +1,8 @@
+def main() -> None:
+    n_classes = 10
+    n_epochs = 20
+    print("DONE")
+
+
+if __name__ == "__main__":
+    main()
