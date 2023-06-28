c6a4a2a2a24ce1aad4183006b2b8fd98b34c03fb
wararaki <ky7.ott.w@gmail.com>
2023-06-28 23:26:11 +0900

    update

--- ml_sample/contrasive_loss/distance.py
+++ ml_sample/contrasive_loss/distance.py
@@ -2,16 +2,18 @@ import torch
 
 
 def euclidean_distance(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    return (x - y).pow(2).sum().sqrt()
+    return (x - y).pow(2).sum(1).sqrt()
 
 def l1_distance(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    return (x - y).abs().sum()
+    return (x - y).abs().sum(1)
 
 
 if __name__ == "__main__":
     a = torch.tensor([[1, 2, 3, 4], [3, 4, 5, 6], [6, 7, 8, 9]])
     c = torch.tensor([[1, 2, 3, 4], [3, 4, 5, 6], [6, 7, 8, 9]])
     b = torch.tensor([[3, 4, 5, 6], [1, 2, 3, 4], [6, 7, 8, 9]])
+    d = torch.tensor([1, 2, 3, 4, 5]).reshape(-1, 1)
+    e = torch.tensor([1, 2, 3, 4, 6]).reshape(-1, 1)
     print(euclidean_distance(a, b))
     print(euclidean_distance(a, c))
     print(l1_distance(a, b))
@@ -20,3 +22,4 @@ if __name__ == "__main__":
     print(result)
     print(a.max(1).values)
     print(torch.max(a, dim=1))
+    print(euclidean_distance(d, e))
--- ml_sample/contrasive_loss/loss.py
+++ ml_sample/contrasive_loss/loss.py
@@ -21,5 +21,5 @@ class ContrasiveLoss(nn.Module):
         # y_true=0 is same class
         # y_true=1 is different class.
         D = self._distance(y_true, y_pred)
-        L = (1. - y_true) * 0.5 * D**2 + y_true * 0.5 * max(0., (self._margin - D.item())**2)
+        L = (1. - y_true.flatten()) * 0.5 * D**2 + y_true.flatten() * 0.5 * torch.relu(self._margin - D) ** 2
         return L
