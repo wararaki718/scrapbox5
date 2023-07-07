c251a00dcf25d15e8ed9b2b75f2f65e5d5c0b60f
wararaki <ky7.ott.w@gmail.com>
2023-07-07 20:55:30 +0900

    update

--- nlp_sample/splade_embedding/main.py
+++ nlp_sample/splade_embedding/main.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from typing import Dict, List, Tuple
 
+import numpy as np
 import torch
 from splade.models.transformer_rep import Splade
 from transformers import AutoTokenizer
@@ -26,6 +27,28 @@ def create_index(rows: List[torch.Tensor], cols: List[torch.Tensor], data: List[
     return index2docid, index2docvalue
         
 
+def retrieve(index2docid: Dict[int, int], index2docvalue: Dict[int, float], cols: List[int], values: List[float], threshold: float=0.) -> Tuple[List[int], List[float]]:
+    scores = np.zeros(len(index2docid), np.float32)
+    n = len(cols)
+    for i in range(n):
+        query_index = cols[i]
+        x_query = values[i]
+        doc_indices = index2docid[query_index]
+        x_docs = index2docvalue[query_index]
+        for index, x_doc in zip(doc_indices, x_docs):
+            scores[index] += x_query * x_doc
+        
+    results = np.argwhere(scores > threshold)[:, 0]
+    return results, scores[results]
+
+
+def select_topk(indices: List[int], scores: List[float], top_k: int=1) -> Tuple[List[int], List[float]]:
+    new_indices = np.argpartition(scores, top_k)[::-1][:top_k]
+    indices = indices[new_indices]
+    scores = scores[new_indices]
+    return indices, scores
+
+
 def main() -> None:
     model_name = "naver/splade-cocondenser-selfdistil"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -39,6 +62,10 @@ def main() -> None:
         "This is an apple. The apple is red.",
         "This is an orange. The orange is orange"
     ]
+    queries = [
+        "berry red",
+        "mango yellow"
+    ]
 
     model.eval()
     with torch.no_grad():
@@ -51,7 +78,6 @@ def main() -> None:
         print(document_embeddings["d_rep"][rows, columns].shape)
         print()
         # show(columns.squeeze().cpu().tolist(), id2vocab)
-
         data = document_embeddings["d_rep"][rows, columns].tolist()
 
         # row, col, data, len(ids)
@@ -61,10 +87,27 @@ def main() -> None:
         print(index2docvalue)
         print()
 
-        query_embedding = model(q_kwargs=tokens)
-        print(query_embedding.keys())
-        print(query_embedding["q_rep"].shape)
-        print(query_embedding["q_rep"].squeeze().shape) # the dimension is the number of vocabs
+        tokens = tokenizer(queries, padding=True, truncation=True, return_tensors="pt", max_length=500)
+        query_embeddings = model(q_kwargs=tokens)
+        rows, columns = torch.nonzero(query_embeddings["q_rep"], as_tuple=True)
+        print(query_embeddings.keys())
+        print(query_embeddings["q_rep"].shape)
+        print(query_embeddings["q_rep"].squeeze().shape) # the dimension is the number of vocabs
+        print()
+
+        data = query_embeddings["q_rep"][rows, columns].tolist()
+        results, scores = retrieve(index2docid, index2docvalue, columns.tolist(), data)
+        print(results)
+        print()
+        print(scores)
+        print()
+
+        results, scores = select_topk(results, scores)
+        print(results)
+        print()
+        print(scores)
+        print()
+        
     print("DONE")
 
 
