dfcabf95ca48550bc66b302dc49f2ba586369b25
Kyohei Watarai <>
2023-07-11 12:58:40 +0900

    update

--- py_sample/sample_loader_iterator/main.py
+++ py_sample/sample_loader_iterator/main.py
@@ -5,21 +5,25 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
 
+class CustomFilter:
+    def __call__(self, text: str) -> bool:
+        return len(text) % 2 == 1
+
+
 class CustomIterator:
-    def __init__(self, data_directory: Path) -> Generator:
+    def __init__(self, data_directory: Path) -> None:
         self._data_directory = data_directory
+        self._custom_filter = CustomFilter()
     
     def __iter__(self) -> Generator[str, None, None]:
         for filepath in self._data_directory.iterdir():
             df = pd.read_csv(filepath)
-            for row in df.itertuples():
-                if len(row.token) % 2 == 0:
-                    continue
-                yield str(row.token)
+            for token in filter(self._custom_filter, df.token):
+                yield token
 
 
 def main() -> None:
-    data_directory = Path("data")
+    data_directory = Path("sample")
     custom_iterator = CustomIterator(data_directory)
 
     for user_id in custom_iterator:
